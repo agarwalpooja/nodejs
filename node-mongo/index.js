df35de406974db5a55b7994d7131b5ade852b65d
agarwalpooja <poojaag1606@gmail.com>
2018-06-20 13:22:48 +0530

    mongo example part 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,34 +1,62 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
+
 
 const url = 'mongodb://localhost:27017/conFusion';
 
 MongoClient.connect(url, (err, db) => {
 
     assert.equal(err,null);
-    
-    console.log('Connected correctly to server');
     var dbase = db.db("conFusion");
-    const collection = dbase.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
+    console.log('Connected correctly to server');
+    dboper.insertDocument(dbase, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+            dboper.findDocuments(dbase, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+                dboper.updateDocument(dbase, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
 
-            dbase.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+                        dboper.findDocuments(dbase, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+                            
+                            dbase.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
 
-                db.close();
+                                db.close();
+                            });
+                        });
+                    });
             });
-        });
     });
+    
+    // var dbase = db.db("conFusion");
+    // const collection = dbase.collection("dishes");
+    // collection.insertOne({"name": "Uthappizza", "description": "test"},
+    // (err, result) => {
+    //     assert.equal(err,null);
+
+    //     console.log("After Insert:\n");
+    //     console.log(result.ops);
+
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err,null);
+            
+    //         console.log("Found:\n");
+    //         console.log(docs);
+
+    //         dbase.dropCollection("dishes", (err, result) => {
+    //             assert.equal(err,null);
+
+    //             db.close();
+    //         });
+    //     });
+    // });
+
 
 });
